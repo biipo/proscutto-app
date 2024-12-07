a37bdce37bc4d60f33459c4a2dc172a121f33b78
biipo <filippo@localhost.localdomain>
2024-12-07 11:28:09 +0100

    Finished bottom navigation bar

--- app/src/main/java/com/ingegneria/app/navigation/Navigation.kt
+++ app/src/main/java/com/ingegneria/app/navigation/Navigation.kt
@@ -6,29 +6,42 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.ingegneria.app.ui.screens.Home
 import com.ingegneria.app.ui.screens.Login
+import com.ingegneria.app.ui.screens.Quiz
+import com.ingegneria.app.ui.screens.Settings
 import com.ingegneria.app.ui.screens.Signup
+import com.ingegneria.app.ui.screens.Tasks
 
 @Composable
 fun Navigation(navController: NavHostController, startScreen:String) {
 
     NavHost(navController = navController, startDestination = startScreen) {
-        composable(Screens.Home.name) {
-            Home(navController = navController)
-        }
         composable(Screens.Login.name) {
             Login(navController = navController)
         }
         composable(Screens.Signup.name) {
             Signup(navController = navController)
         }
+        // TODO: forgot password
+        composable(Screens.Home.name) {
+            Home(navController = navController)
+        }
         composable(Screens.Tasks.name) {
-            Signup(navController = navController)
+            Tasks(navController = navController)
         }
         composable(Screens.Quiz.name) {
-            Signup(navController = navController)
+            Quiz(navController = navController)
         }
         composable(Screens.Settings.name) {
-            Signup(navController = navController)
+            Settings(navController = navController)
         }
+//        composable(Screens.Quiz.name) {
+//            Social(navController = navController)
+//        }
+//        composable(Screens.Quiz.name) {
+//            Shop(navController = navController)
+//        }
+//        composable(Screens.Quiz.name) {
+//            Stats(navController = navController)
+//        }
     }
 }
