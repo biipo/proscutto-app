b65563d3ec08f37544a7f32282211482fd71c6bb
l0wfi <37485103+l0wfi@users.noreply.github.com>
2024-12-07 19:36:59 +0100

    Removed bar visibility in login

--- app/src/main/java/com/ingegneria/app/MainActivity.kt
+++ app/src/main/java/com/ingegneria/app/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ingegneria.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -21,19 +22,27 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.saveable.rememberSaveableStateHolder
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import com.ingegneria.app.navigation.Navigation
 import com.ingegneria.app.navigation.Screens
 import com.ingegneria.app.ui.theme.AppTheme
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 
 class MainActivity : ComponentActivity() {
 
@@ -59,8 +68,22 @@ fun ProscuttoApp(navController: NavHostController = rememberNavController()) {
         } else {
             Screens.Home.name
         }
+    val bottomBarState = rememberSaveable { mutableStateOf(true) }
+    val topBarState = rememberSaveable { mutableStateOf(true) }
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+
+    when (navBackStackEntry?.destination?.route) {
+        Screens.Login.name -> {
+            bottomBarState.value = false
+        }
+        Screens.Home.name -> {
+            bottomBarState.value = true
+        }
+    }
+
     Scaffold (
-        bottomBar = { TabNavigationBar(navController) }
+        bottomBar = { TabNavigationBar(navController, bottomBarState) }
     ) { padding ->
         Column (
             modifier = Modifier.padding(padding)
@@ -71,7 +94,7 @@ fun ProscuttoApp(navController: NavHostController = rememberNavController()) {
 }
 
 @Composable
-fun TabNavigationBar(navController: NavHostController) {
+fun TabNavigationBar(navController: NavHostController, bottomBarState: MutableState<Boolean>) {
     var selectedItem by remember { mutableIntStateOf(0) }
     var items = listOf(Screens.Home.name, Screens.Tasks.name, Screens.Quiz.name, Screens.Settings.name)
     val selectedIcons = listOf(Icons.Filled.Home,
@@ -83,22 +106,28 @@ fun TabNavigationBar(navController: NavHostController) {
                                  Icons.Outlined.Quiz,
                                  Icons.Outlined.Settings)
 
-    NavigationBar {
-        items.forEachIndexed { index: Int, item ->
-            NavigationBarItem(
-                icon = {
-                    Icon(
-                        if (selectedItem == index) selectedIcons[index] else unselectedIcons[index],
-                        contentDescription = item
-                    )
-                },
-                label = { Text(item) },
-                selected = selectedItem == index,
-                onClick = {
-                    selectedItem = index
-                    navController.navigate(item)
-                }
-            )
+    AnimatedVisibility(
+        visible = bottomBarState.value,
+        enter = slideInVertically(initialOffsetY = {it}),
+        exit = slideOutVertically(targetOffsetY = {it})
+    ) {
+        NavigationBar {
+            items.forEachIndexed { index: Int, item ->
+                NavigationBarItem(
+                    icon = {
+                        Icon(
+                            if (selectedItem == index) selectedIcons[index] else unselectedIcons[index],
+                            contentDescription = item
+                        )
+                    },
+                    label = { Text(item) },
+                    selected = selectedItem == index,
+                    onClick = {
+                        selectedItem = index
+                        navController.navigate(item)
+                    }
+                )
+            }
         }
     }
 }
