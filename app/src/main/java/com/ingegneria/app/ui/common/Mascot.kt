95e53ddb72e8feaf35d7b4f6ea14c640b4cfd6df
l0wfi <37485103+l0wfi@users.noreply.github.com>
2024-12-07 15:08:48 +0100

    Put mascot image in box

--- app/src/main/java/com/ingegneria/app/ui/common/Mascot.kt
+++ app/src/main/java/com/ingegneria/app/ui/common/Mascot.kt
@@ -1,6 +1,8 @@
 package com.ingegneria.app.ui.common
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -9,8 +11,10 @@ import com.ingegneria.app.R
 @Composable
 fun MascotImage(modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.tartegas320)
-    Image (
-        painter = image,
-        contentDescription = null
-    )
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+    }
 }
