71ef2f67dfee7bcc055c6ff32b6468a56605bdca
l0wfi <37485103+l0wfi@users.noreply.github.com>
2024-12-07 22:05:02 +0100

    Added character name and mascot to home

--- app/src/main/java/com/ingegneria/app/ui/common/Mascot.kt
+++ app/src/main/java/com/ingegneria/app/ui/common/Mascot.kt
@@ -2,7 +2,6 @@ package com.ingegneria.app.ui.common
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -11,6 +10,16 @@ import com.ingegneria.app.R
 @Composable
 fun MascotImage(modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.tartegas320)
+    Box (modifier) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+    }
+}
+@Composable
+fun MascotImageBig(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.tartegas512)
     Box (modifier) {
         Image(
             painter = image,
--- app/src/main/java/com/ingegneria/app/ui/screens/Home.kt
+++ app/src/main/java/com/ingegneria/app/ui/screens/Home.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -12,10 +13,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.People
 import androidx.compose.material.icons.filled.ShoppingCart
-import androidx.compose.material.icons.rounded.Person
-import androidx.compose.material.icons.rounded.ShoppingCart
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -24,18 +23,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import androidx.compose.material3.TopAppBar
-import androidx.compose.ui.unit.sp
+import com.ingegneria.app.ui.common.MascotImageBig
+import com.ingegneria.app.ui.theme.AppTheme
 
 @Composable
 fun Home(navController: NavController) {
@@ -71,14 +72,14 @@ fun TopAppBar () {
                         onClick = {}
                     ) {
                         Icon(
-                            imageVector = Icons.Default.Person,
+                            imageVector = Icons.Default.People,
                             contentDescription = "Social button"
                         )
                     }
                 }
             )
         },
-    ) {padding ->
+    ) { padding ->
         Column (
             modifier = Modifier.padding(padding)
         ) {}
@@ -93,62 +94,76 @@ fun CharacterStats() {
     val currentXpExample = 140
     val maxHp = 300
     val maxXp = 200
-    Row (
+    val characterName = "Artemisia"
+
+    Column (
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 130.dp, start = 15.dp, end = 15.dp)
+            .fillMaxHeight()
     ) {
-        Box (
+        Row (
             modifier = Modifier
-                .size(100.dp)
-                .clip(RoundedCornerShape(15.dp))
-                .background(MaterialTheme.colorScheme.primaryContainer),
-            contentAlignment = Alignment.Center
+                .padding(top = 90.dp, start = 15.dp, end = 15.dp)
         ) {
             Text(
-                text = "$lvlExample",
-                fontSize = 50.sp
+                text = characterName,
+                fontSize = 40.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
             )
+        }
+        Row(
+            modifier = Modifier
+                .padding(top = 20.dp, start = 15.dp, end = 15.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(RoundedCornerShape(15.dp))
+                    .background(MaterialTheme.colorScheme.primaryContainer),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "$lvlExample",
+                    fontSize = 50.sp
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .padding(start = 10.dp)
+            ) {
+                Text(
+                    text = "$currentHpExample/$maxHp",
+                    fontSize = 15.sp,
+                    modifier = Modifier.align(Alignment.End)
+                )
+                LinearProgressIndicator(
+                    progress = { (currentHpExample / maxHp.toFloat()) },
+                    modifier = Modifier.fillMaxWidth().height(15.dp),
+                    color = MaterialTheme.colorScheme.primary,
+                    trackColor = MaterialTheme.colorScheme.background
+                )
+                Text(
+                    text = "$currentXpExample/$maxXp",
+                    fontSize = 15.sp,
+                    modifier = Modifier.align(Alignment.End)
+                        .padding(0.dp, 15.dp, 0.dp, 0.dp)
+                )
+                LinearProgressIndicator(
+                    progress = { (currentXpExample / maxXp.toFloat()) },
+                    modifier = Modifier.fillMaxWidth().height(15.dp),
+                    color = MaterialTheme.colorScheme.tertiary,
+                    trackColor = MaterialTheme.colorScheme.background
+                )
+            }
         }
         Column (
-            modifier = Modifier
-                .padding(start = 10.dp)
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxHeight().fillMaxWidth()
         ) {
-            // TODO: show text over the right corner of the bar
-            Text(
-                text = "$currentHpExample/$maxHp",
-                fontSize = 15.sp,
-                modifier = Modifier.align(Alignment.End)
-            )
-            LinearProgressIndicator(
-                progress = { (currentHpExample/maxHp.toFloat()) },
-                modifier = Modifier.fillMaxWidth().height(15.dp),
-                color = MaterialTheme.colorScheme.primary,
-                trackColor = MaterialTheme.colorScheme.background
-            )
-            Text(
-                text = "$currentXpExample/$maxXp",
-                fontSize = 15.sp,
-                modifier = Modifier.align(Alignment.End)
-                    .padding(0.dp, 15.dp, 0.dp, 0.dp)
-            )
-            LinearProgressIndicator(
-                progress = { (currentXpExample/maxXp.toFloat()) },
-                modifier = Modifier.fillMaxWidth().height(15.dp),
-                color = MaterialTheme.colorScheme.tertiary,
-                trackColor = MaterialTheme.colorScheme.background
-            )
-            /*
-            Box (
-                modifier = Modifier
-                    .padding(top = 20.dp)
-                    .size(width = 300.dp, height = 30.dp)
-                    .clip(RoundedCornerShape(8.dp))
-                    .background(Color.Blue)
-            )*/
-
+            MascotImageBig()
         }
-
     }
 }
 
@@ -156,5 +171,5 @@ fun CharacterStats() {
 @Preview(showBackground = true)
 @Composable
 fun PreviewHome(navController: NavController = rememberNavController()){
-    Home(navController)
+    AppTheme { Home(navController) }
 }
