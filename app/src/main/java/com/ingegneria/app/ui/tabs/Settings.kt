23ab074b97ec57bb2ddf83534bcba9d97694185e
Giomaro980 <887931@stud.unive.it>
2025-01-28 20:50:53 +0100

    aggiunto deleteacc

--- app/src/main/java/com/ingegneria/app/ui/tabs/Settings.kt
+++ app/src/main/java/com/ingegneria/app/ui/tabs/Settings.kt
@@ -1,15 +1,10 @@
 package com.ingegneria.app.ui.tabs
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import android.content.ContentValues.TAG
+import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -18,14 +13,15 @@ import androidx.navigation.compose.rememberNavController
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 
-
 @Composable
 fun Settings(navController: NavController) {
+    var showDialog by remember { mutableStateOf(false) }
+
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        Column (
+        Column(
             modifier = Modifier
                 .fillMaxHeight(),
         ) {
@@ -42,12 +38,50 @@ fun Settings(navController: NavController) {
             ) {
                 Text("LOGOUT")
             }
+            Spacer(modifier = Modifier.padding(5.dp))
+            Button(
+                modifier = Modifier
+                    .padding(vertical = 2.dp)
+                    .fillMaxWidth(),
+                onClick = {
+                    showDialog = true
+                }
+            ) {
+                Text("DELETE ACCOUNT")
+            }
         }
+    }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            confirmButton = {
+                TextButton(onClick = {
+                    showDialog = false
+                    val user = Firebase.auth.currentUser!!
+                    user.delete()
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Log.d(TAG, "User account deleted.")
+                            }
+                        }
+                }) {
+                    Text("CONFIRM")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { showDialog = false }) {
+                    Text("CANCEL")
+                }
+            },
+            title = { Text("Delete Account") },
+            text = { Text("Are you sure you want to delete your account? This action cannot be undone.") }
+        )
     }
 }
 
 @Preview
 @Composable
-fun PreviewSettings(navController: NavController = rememberNavController()){
+fun PreviewSettings(navController: NavController = rememberNavController()) {
     Settings(navController = navController)
 }
