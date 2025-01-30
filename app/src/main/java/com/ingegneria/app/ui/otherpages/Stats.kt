c7784bedbdd90ad0e52de5b8ad27d2bf57b0dc1d
biipo <filippo@localhost.localdomain>
2025-01-30 19:21:45 +0100

    Improved stats page UI

--- app/src/main/java/com/ingegneria/app/ui/otherpages/Stats.kt
+++ app/src/main/java/com/ingegneria/app/ui/otherpages/Stats.kt
@@ -2,12 +2,22 @@ package com.ingegneria.app.ui.otherpages
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -19,6 +29,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -78,16 +89,91 @@ fun Stats(navController: NavController) {
                 .fillMaxSize()
                 .padding(padding),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
         ){
-            Text(text = "Stats Page", modifier = Modifier.padding(bottom = 20.dp))
+            Column (
+                modifier = Modifier.wrapContentSize(),
+                verticalArrangement = Arrangement.Top,
+            ){
+                Text(
+                    modifier = Modifier.padding(top = 10.dp),
+                    text = "Statistics",
+                    style = MaterialTheme.typography.displayMedium
+                )
+            }
 
-            Text(text = "Max Level Reached: $maxLevelReached")
-            Text(text = "Daily Tasks Completed: $dailyTasksCompleted")
-            Text(text = "Weekly Tasks Completed: $weeklyTasksCompleted")
-            Text(text = "Monthly Tasks Completed: $monthlyTasksCompleted")
-            Text(text = "Quiz Completed: $quizCompleted")
-            Text(text = "Days Logged In: $totalLoginDays")
+            HorizontalDivider(
+                modifier = Modifier.padding(horizontal = 15.dp, vertical = 15.dp),
+                thickness = 2.dp,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            LazyColumn (
+                modifier = Modifier.wrapContentSize(),
+                verticalArrangement = Arrangement.Center
+            ){
+                item {
+                    StatCard("Max Level Reached", maxLevelReached.toString())
+                }
+                item {
+                    StatCard("Daily Tasks Completed", dailyTasksCompleted.toString())
+                }
+                item {
+                    StatCard("Weekly Tasks Completed", weeklyTasksCompleted.toString())
+                }
+                item {
+                    StatCard("Monthly Tasks Completed", monthlyTasksCompleted.toString())
+                }
+                item {
+                    StatCard("Quiz Completed", quizCompleted.toString())
+                }
+                item {
+                    StatCard("Days Logged In", totalLoginDays.toString())
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+}
+@Composable
+fun StatCard(title: String, value: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .shadow(8.dp),
+        shape = RoundedCornerShape(8.dp),
+        colors = CardColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer,
+            contentColor = MaterialTheme.colorScheme.onSecondaryContainer,
+            disabledContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+            disabledContentColor = MaterialTheme.colorScheme.onSecondaryContainer
+        )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Card(
+                modifier = Modifier.padding(end = 5.dp).wrapContentSize(),
+                colors = CardColors(
+                    containerColor = MaterialTheme.colorScheme.secondary,
+                    contentColor = MaterialTheme.colorScheme.onSecondary,
+                    disabledContainerColor = MaterialTheme.colorScheme.secondary,
+                    disabledContentColor = MaterialTheme.colorScheme.onSecondary
+                )
+            ) {
+                Text(
+                    modifier = Modifier.padding(10.dp),
+                    text = value,
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
         }
     }
 }
